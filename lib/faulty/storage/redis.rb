611b3c4e7c0118bc673740bbb6bb05f979b9b611
Justin Howard <jmhoward0@gmail.com>
2021-02-18 20:49:49 -0800

    Fix Redis storage dependency on ConnectionPool

--- lib/faulty/storage/redis.rb
+++ lib/faulty/storage/redis.rb
@@ -384,7 +384,7 @@ class Faulty
       end
 
       def check_pool_options!
-        if options.client.is_a?(ConnectionPool)
+        if options.client.class.name == 'ConnectionPool'
           timeout = options.client.instance_variable_get(:@timeout)
           warn(<<~MSG) if timeout > 2
             Faulty recommends setting ConnectionPool timeouts <= 2 to prevent
